5306ecf912b6214ddaaa71ccfebed035f1c2051d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-05-06 20:36:43 +0200

    Add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { GithubUserWidgetModule } from 'github-user-widget/src/public-api';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {GithubUserWidgetModule} from 'github-user-widget';
 
 @NgModule({
   declarations: [
